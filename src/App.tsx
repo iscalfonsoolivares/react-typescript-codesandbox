a7307296a550a2f8be82139ce1d054de0058822c
iscalfonsoolivares <contacto@iscalfonsoolivares.com>
2021-01-25 12:14:11 -0600

    Adding dependencies Adding some dependencies to test

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
 import "./styles.css";
+import React, { useEffect } from "react";
+
+import axios from "axios";
+
+interface User {
+  id: number;
+}
 
 export default function App() {
+  useEffect(() => {
+    axios
+      .get<User[]>("https://box.aowebdev.com/api/users.json")
+      .then((response) => {
+        console.log(response.data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <h1>Everything is awesome</h1>
